3e6a29741dcf0bcdebdeca64ced7a9fa0c7e9c4d
Loreen.Roose1 <loreen.roose@finanzen.bremen.de>
2022-09-05 16:18:43 +0200

    Page to show timetable can be loaded (can't show timetable yet), is shown in a separate window currently

--- src/de/bremen/jTimetable/fxmlController/HomeController.java
+++ src/de/bremen/jTimetable/fxmlController/HomeController.java
@@ -1,10 +1,12 @@
 package de.bremen.jTimetable.fxmlController;
 
 //import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import de.bremen.jTimetable.Classes.CourseofStudy;
 import de.bremen.jTimetable.Classes.Coursepass;
 import de.bremen.jTimetable.Main;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,7 +22,9 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -28,29 +32,51 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
-    @FXML private Button ActiveCoursesofStudyButtonNew;
-    @FXML private Button ActiveCoursesofStudyButton;
-    @FXML private TableView<CourseofStudy> ActiveCoursesofStudyTableview;
-    @FXML private TableColumn<CourseofStudy, Long> COSID;
-    @FXML private TableColumn<CourseofStudy, String> COSDescription;
-    @FXML private TableColumn<CourseofStudy, LocalDate> COSBegin;
-    @FXML private TableColumn<CourseofStudy, LocalDate> COSEnd;
-    @FXML private TableColumn<CourseofStudy, Boolean> COSActive;
-    @FXML private CheckBox chkToogleActiveCourseofStudy;
-    @FXML private TableView<Coursepass> CoursepassTableview;
-    @FXML private TableColumn<Coursepass, Long> CPID;
-    @FXML private TableColumn<Coursepass, String> CPCOSCaption;
-    @FXML private TableColumn<Coursepass, String> CPstudysection;
-    @FXML private TableColumn<Coursepass, String> CPDescription;
-    @FXML private TableColumn<Coursepass, LocalDate> CPStart;
-    @FXML private TableColumn<Coursepass, LocalDate> CPEnd;
-    @FXML private TableColumn<Coursepass, Boolean> CPActive;
-    @FXML private Button btnCoursepassEdit;
-    @FXML private Button btnCoursepassNew;
-    @FXML private CheckBox chkToogleCoursepass;
-    public HomeController(){
+    @FXML
+    private Button ActiveCoursesofStudyButtonNew;
+    @FXML
+    private Button ActiveCoursesofStudyButton;
+    @FXML
+    private TableView<CourseofStudy> ActiveCoursesofStudyTableview;
+    @FXML
+    private TableColumn<CourseofStudy, Long> COSID;
+    @FXML
+    private TableColumn<CourseofStudy, String> COSDescription;
+    @FXML
+    private TableColumn<CourseofStudy, LocalDate> COSBegin;
+    @FXML
+    private TableColumn<CourseofStudy, LocalDate> COSEnd;
+    @FXML
+    private TableColumn<CourseofStudy, Boolean> COSActive;
+    @FXML
+    private CheckBox chkToogleActiveCourseofStudy;
+    @FXML
+    private TableView<Coursepass> CoursepassTableview;
+    @FXML
+    private TableColumn<Coursepass, Long> CPID;
+    @FXML
+    private TableColumn<Coursepass, String> CPCOSCaption;
+    @FXML
+    private TableColumn<Coursepass, String> CPstudysection;
+    @FXML
+    private TableColumn<Coursepass, String> CPDescription;
+    @FXML
+    private TableColumn<Coursepass, LocalDate> CPStart;
+    @FXML
+    private TableColumn<Coursepass, LocalDate> CPEnd;
+    @FXML
+    private TableColumn<Coursepass, Boolean> CPActive;
+    @FXML
+    private Button btnCoursepassEdit;
+    @FXML
+    private Button btnCoursepassNew;
+    @FXML
+    private CheckBox chkToogleCoursepass;
+
+    public HomeController() {
 
     }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         COSID.setCellValueFactory(new PropertyValueFactory<CourseofStudy, Long>("id"));
@@ -72,43 +98,43 @@ public class HomeController implements Initializable {
         ActiveCoursesofStudyButton.setOnAction(event -> {
             TableView.TableViewSelectionModel<CourseofStudy> selectionModel = ActiveCoursesofStudyTableview.getSelectionModel();
             ObservableList<CourseofStudy> selectedItems = selectionModel.getSelectedItems();
-            if (selectedItems.size() > 0){
+            if (selectedItems.size() > 0) {
                 //System.out.println(selectedItems.get(0).getId());
-                Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                Stage stageTheEventSourceNodeBelongs = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 FXMLLoader loader = new FXMLLoader();
                 URL url = Main.class.getResource("fxml/CourseofStudy.fxml");
                 loader.setLocation(url);
-                try{
+                try {
                     AnchorPane anchorPane = loader.<AnchorPane>load();
                     CourseofStudyController courseofStudyController = loader.<CourseofStudyController>getController();
                     courseofStudyController.setID(new CourseofStudy(selectedItems.get(0).getId()));
                     Scene scene = new Scene(anchorPane);
                     stageTheEventSourceNodeBelongs.setScene(scene);
-                }catch (Exception e ){
+                } catch (Exception e) {
                     //TODo: Propper Error handling
                     System.out.println(e);
                 }
             }
         });
 
-        ActiveCoursesofStudyButtonNew.setOnAction(event ->{
-            Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        ActiveCoursesofStudyButtonNew.setOnAction(event -> {
+            Stage stageTheEventSourceNodeBelongs = (Stage) ((Node) event.getSource()).getScene().getWindow();
             FXMLLoader loader = new FXMLLoader();
             URL url = Main.class.getResource("fxml/CourseofStudy.fxml");
             loader.setLocation(url);
-            try{
+            try {
                 AnchorPane anchorPane = loader.<AnchorPane>load();
                 CourseofStudyController courseofStudyController = loader.<CourseofStudyController>getController();
                 courseofStudyController.setID(new CourseofStudy(0L));
                 Scene scene = new Scene(anchorPane);
                 stageTheEventSourceNodeBelongs.setScene(scene);
-            }catch (Exception e ){
+            } catch (Exception e) {
                 //TODo: Propper Error handling
                 System.out.println(e);
             }
         });
 
-        chkToogleActiveCourseofStudy.setOnAction(event ->{
+        chkToogleActiveCourseofStudy.setOnAction(event -> {
             ActiveCoursesofStudyTableview.getItems().setAll(getCoursesofStudy(!chkToogleActiveCourseofStudy.isSelected()));
         });
 
@@ -124,63 +150,84 @@ public class HomeController implements Initializable {
         CoursepassTableview.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent click) {
+                //DoubleClick: Editor is opened
                 if (click.getClickCount() == 2) {
                     btnCoursepassEdit.fire();
+                }
+                //SingleClick: Timetable is shown
+                if (click.getClickCount() == 1) {
+                    TableView.TableViewSelectionModel<Coursepass> selectionModel = CoursepassTableview.getSelectionModel();
+                    ObservableList<Coursepass> selectedItems = selectionModel.getSelectedItems();
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/TimetableView.fxml"));
+                    Stage stage = new Stage(StageStyle.DECORATED);
+                    URL url = Main.class.getResource("fxml/TimetableView.fxml");
+                    loader.setLocation(url);
+                    try {
+                        stage.setScene(new Scene(loader.load()));
+                        stage.setTitle("Timetable");
+                        TimetableViewController controller = loader.getController();
+                        controller.initData(new Coursepass((selectedItems.get(0).getId())));
+                        stage.show();
+                    } catch (SQLException | IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         });
 
-        btnCoursepassEdit.setOnAction(event ->{
+        btnCoursepassEdit.setOnAction(event -> {
             TableView.TableViewSelectionModel<Coursepass> selectionModel = CoursepassTableview.getSelectionModel();
             ObservableList<Coursepass> selectedItems = selectionModel.getSelectedItems();
-            if (selectedItems.size() > 0){
+            if (selectedItems.size() > 0) {
                 //System.out.println(selectedItems.get(0).getId());
-                Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                Stage stageTheEventSourceNodeBelongs = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 FXMLLoader loader = new FXMLLoader();
                 URL url = Main.class.getResource("fxml/Coursepass.fxml");
                 loader.setLocation(url);
-                try{
+                try {
                     AnchorPane anchorPane = loader.<AnchorPane>load();
                     CoursepassController coursepassController = loader.<CoursepassController>getController();
                     coursepassController.setCoursepass(new Coursepass(selectedItems.get(0).getId()));
                     Scene scene = new Scene(anchorPane);
                     stageTheEventSourceNodeBelongs.setScene(scene);
-                }catch (Exception e ){
+                } catch (Exception e) {
                     //TODo: Propper Error handling
                     System.out.println(e);
                 }
             }
         });
 
-        btnCoursepassNew.setOnAction(event ->{
+        btnCoursepassNew.setOnAction(event -> {
             TableView.TableViewSelectionModel<Coursepass> selectionModel = CoursepassTableview.getSelectionModel();
             ObservableList<Coursepass> selectedItems = selectionModel.getSelectedItems();
-            if (selectedItems.size() > 0){
+            if (selectedItems.size() > 0) {
                 //System.out.println(selectedItems.get(0).getId());
-                Stage stageTheEventSourceNodeBelongs = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                Stage stageTheEventSourceNodeBelongs = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 FXMLLoader loader = new FXMLLoader();
                 URL url = Main.class.getResource("fxml/Coursepass.fxml");
                 loader.setLocation(url);
-                try{
+                try {
                     AnchorPane anchorPane = loader.<AnchorPane>load();
                     CoursepassController coursepassController = loader.<CoursepassController>getController();
                     coursepassController.setCoursepass(new Coursepass(0L));
                     Scene scene = new Scene(anchorPane);
                     stageTheEventSourceNodeBelongs.setScene(scene);
-                }catch (Exception e ){
+                } catch (Exception e) {
                     //TODo: Propper Error handling
                     System.out.println(e);
                 }
             }
         });
 
-        chkToogleCoursepass.setOnAction(event ->{
+        chkToogleCoursepass.setOnAction(event -> {
             CoursepassTableview.getItems().setAll(getCoursepass(!chkToogleCoursepass.isSelected()));
         });
+
     }
+
     public ArrayList<CourseofStudy> getCoursesofStudy(Boolean activeState) {
         ArrayList<CourseofStudy> activeCoursesofStudy = new ArrayList();
-        try{
+        try {
             activeCoursesofStudy = new CourseofStudy(0L).getCoursesofStudy(activeState);
         } catch (SQLException e) {
             //TODo: better error handling
@@ -191,7 +238,7 @@ public class HomeController implements Initializable {
 
     public ArrayList<Coursepass> getCoursepass(Boolean activeState) {
         ArrayList<Coursepass> activeCoursepass = new ArrayList();
-        try{
+        try {
             activeCoursepass = new Coursepass(0L).getCoursepasses(activeState);
         } catch (SQLException e) {
             //TODo: better error handling
--- src/de/bremen/jTimetable/fxmlController/TimetableViewController.java
+++ src/de/bremen/jTimetable/fxmlController/TimetableViewController.java
@@ -1,4 +1,39 @@
 package de.bremen.jTimetable.fxmlController;
 
-public class TimetableViewController {
+import de.bremen.jTimetable.Classes.Coursepass;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TimetableViewController implements Initializable {
+
+    @FXML
+    public ScrollPane scrllpn_TimetableView;
+
+    @FXML
+    public GridPane grdpn_TimetableView;
+
+    private Coursepass coursepass;
+
+    /**
+     * Can be called to hand parameters from the calling class to this controller
+     *
+     * @param coursepass the coursepass that was selected and for which the timetable is shown
+     */
+    public void initData(Coursepass coursepass) {
+        this.coursepass = coursepass;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
 }
