82aeade106828e033a46a5c0559a2e5ee647b55d
arne <arne.czyborra@finanzen.bremen.de>
2023-07-18 21:34:37 +0200

    POM Fix and first Test successfull

---
+++ src/main/java/de/bremen/jTimetable/Classes/ClickableButton.java
@@ -0,0 +1,32 @@
+package de.bremen.jTimetable.Classes;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+// Example Class for TestFX Testing
+public class ClickableButton extends Application {
+
+    // application for acceptance tests.
+    @Override
+    public void start(Stage stage) {
+        Parent sceneRoot = new ClickPane();
+        Scene scene = new Scene(sceneRoot, 100, 100);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // scene object for unit tests
+    public static class ClickPane extends StackPane {
+        public ClickPane() {
+            super();
+            Button button = new Button("click me!!!");
+            button.setOnAction(actionEvent -> button.setText("clicked!"));
+            getChildren().add(button);
+        }
+
+    }
+}
